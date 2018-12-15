8bb80d60da82b4897e0627538a84598abf866b66
pezzu <peter.sukhenko@gmail.com>
2018-12-15 10:21:37 +0200

    API to 1st assignment

---
+++ algorithms/1-percolation/src/Percolation.java
@@ -0,0 +1,31 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+
+public class Percolation {
+
+    // create n-by-n grid, with all sites blocked
+    public Percolation(int n) {
+    }
+
+    // open site (row, col) if it is not open already
+    public void open(int row, int col) {
+    }
+
+    // is site (row, col) open?
+    public boolean isOpen(int row, int col) {
+    }
+
+    // is site (row, col) full?
+    public boolean isFull(int row, int col) {
+    }
+
+    // number of open sites
+    public int numberOfOpenSites() {
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+    }
+}
