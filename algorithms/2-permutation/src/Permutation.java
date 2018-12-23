f81f8cf128dcbaa451bcada53825e918ba396ae2
pezzu <peter.sukhenko@gmail.com>
2018-12-23 02:26:17 +0200

    Added permutation impl

--- algorithms/2-permutation/src/Permutation.java
+++ algorithms/2-permutation/src/Permutation.java
@@ -1,5 +1,21 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Iterator;
+
 public class Permutation {
     public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> queue = new RandomizedQueue<>();
 
+        while (!StdIn.isEmpty()) {
+            String in = StdIn.readString();
+            queue.enqueue(in);
+        }
+
+        Iterator<String> it = queue.iterator();
+        for(int i = 0; i < k; i++) {
+            StdOut.println(it.next());
+        }
     }
 }
