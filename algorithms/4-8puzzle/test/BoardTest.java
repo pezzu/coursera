0aac474ddae56e3623eddebe1eabb5a82a418909
pezzu <peter.sukhenko@gmail.com>
2019-01-04 23:57:58 +0200

    Some more tests

--- algorithms/4-8puzzle/test/BoardTest.java
+++ algorithms/4-8puzzle/test/BoardTest.java
@@ -1,8 +1,19 @@
 import org.testng.annotations.Test;
 
+import java.util.Iterator;
+
 import static org.testng.Assert.*;
 
 public class BoardTest {
+
+    @Test
+    public void constructionBlank() {
+        int[][] blocks = new int[][] {{}};
+
+        Board board = new Board(blocks);
+
+        assertEquals(board.dimension(), 0);
+    }
 
     @Test
     public void dimension() {
@@ -24,6 +35,97 @@ public class BoardTest {
 
         Board board4 = new Board(blocks);
         assertEquals(board4.dimension(), 4);
+    }
+
+    @Test
+    public void hamming() {
+        int[][] blocks = new int[][] {
+                {8, 1, 3},
+                {4, 0, 2},
+                {7, 6, 5}
+        };
+        Board board = new Board(blocks);
+        assertEquals(board.hamming(), 5);
+    }
+
+    @Test
+    public void manhattan() {
+        int[][] blocks = new int[][] {
+                {8, 1, 3},
+                {4, 0, 2},
+                {7, 6, 5}
+        };
+        Board board = new Board(blocks);
+        assertEquals(board.manhattan(), 10);
+    }
+
+    @Test
+    public void isGoalOK() {
+        int[][] blocks = new int[][] {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 0}
+        };
+        Board board = new Board(blocks);
+        assertTrue(board.isGoal());
+    }
+
+    @Test
+    public void isGoalNot() {
+        int[][] blocks = new int[][] {
+                {8, 1, 3},
+                {4, 0, 2},
+                {7, 6, 5}
+        };
+        Board board = new Board(blocks);
+        assertFalse(board.isGoal());
+    }
+
+    @Test
+    public void twin() {
+        int[][] blocks = new int[][] {
+                {8, 1, 3},
+                {4, 0, 2},
+                {7, 6, 5}
+        };
+        Board board = new Board(blocks);
+
+        assertTrue(false);
+    }
+
+    @Test
+    public void neighbors() {
+        int[][] blocks = new int[][] {
+                {8, 1, 3},
+                {4, 2, 0},
+                {7, 6, 5}
+        };
+        Board board = new Board(blocks);
+
+        Iterator<Board> it = board.neighbors().iterator();
+
+        assertTrue(it.hasNext());
+        assertEquals(it.next(), new Board(new int[][] {
+                {8, 1, 0},
+                {4, 2, 3},
+                {7, 6, 5}
+        }));
+
+        assertTrue(it.hasNext());
+        assertEquals(it.next(), new Board(new int[][] {
+                {8, 1, 3},
+                {4, 0, 2},
+                {7, 6, 5}
+        }));
+
+        assertTrue(it.hasNext());
+        assertEquals(it.next(), new Board(new int[][] {
+                {8, 1, 3},
+                {4, 2, 5},
+                {7, 6, 0}
+        }));
+
+        assertFalse(it.hasNext());
     }
 
     @Test
