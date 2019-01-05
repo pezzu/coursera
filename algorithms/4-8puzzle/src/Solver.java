8e5e79cfd196669563740e43d6ca1324501204ed
pezzu <peter.sukhenko@gmail.com>
2019-01-05 23:34:13 +0200

    Initial implementation of Solver

--- algorithms/4-8puzzle/src/Solver.java
+++ algorithms/4-8puzzle/src/Solver.java
@@ -1,25 +1,64 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Queue;
+
+import java.util.Comparator;
 
 public class Solver {
+
+    private final int moves;
+    private final Queue<Board> solution = new Queue<Board>();
+
+    private static final Comparator<Board> BY_MANHATTAN = new ByManhattan();
+
+    private static class ByManhattan implements Comparator<Board> {
+        public int compare(Board a, Board b) {
+            return a.manhattan() - b.manhattan();
+        }
+    }
+
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
+        MinPQ<Board> allMoves = new MinPQ<Board>(BY_MANHATTAN);
+        allMoves.insert(initial);
 
+        Board min = allMoves.delMin();
+        Board predecessor = null;
+        int numberOfMoves = 0;
+
+        while(!min.isGoal()) {
+
+            for(Board neighbor: min.neighbors()) {
+                if(!neighbor.equals(predecessor)){
+                    allMoves.insert(neighbor);
+                }
+            }
+
+            predecessor = min;
+            solution.enqueue(min);
+            min = allMoves.delMin();
+            numberOfMoves++;
+        }
+        solution.enqueue(min);
+
+        this.moves = numberOfMoves;
     }
 
     // is the initial board solvable?
     public boolean isSolvable() {
-        return false;
+        return moves >= 0;
     }
 
     // min number of moves to solve initial board; -1 if unsolvable
     public int moves() {
-        return 0;
+        return moves;
     }
 
     // sequence of boards in a shortest solution; null if unsolvable
     public Iterable<Board> solution() {
-        return null;
+        // ToDo: defensive copy
+        return solution;
     }
 
     // solve a slider puzzle (given below)
--- algorithms/4-8puzzle/test/SolverTest.java
+++ algorithms/4-8puzzle/test/SolverTest.java
@@ -62,7 +62,107 @@ public class SolverTest {
     }
 
     @Test
-    public void solution() {
+    public void solutionNone() {
+        int[][] blocks = new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {8, 7, 0}
+        };
+
+        Solver solver = new Solver(new Board(blocks));
+        assertNull(solver.solution());
+    }
+
+    @Test
+    public void puzzle00() {
+        Board board = new Board(new int[][]{
+                {1,  2,  3,  4,  5,  6,  7,  8,  9,  10},
+                {11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+                {21, 22, 23, 24, 25, 26, 27, 28, 29, 30},
+                {31, 32, 33, 34, 35, 36, 37, 38, 39, 40},
+                {41, 42, 43, 44, 45, 46, 47, 48, 49, 50},
+                {51, 52, 53, 54, 55, 56, 57, 58, 59, 60},
+                {61, 62, 63, 64, 65, 66, 67, 68, 69, 70},
+                {71, 72, 73, 74, 75, 76, 77, 78, 79, 80},
+                {81, 82, 83, 84, 85, 86, 87, 88, 89, 90},
+                {91, 92, 93, 94, 95, 96, 97, 98, 99, 0}
+        });
+
+        Solver solver = new Solver(board);
+
+        assertTrue(solver.isSolvable());
+        assertEquals(solver.moves(), 0);
+
+        Iterator<Board> it = solver.solution().iterator();
+
+        assertTrue(it.hasNext());
+        assertEquals(it.next(), board);
+        assertFalse(it.hasNext());
+    }
+
+    @Test
+    public void puzzle01() {
+        Board board = new Board(new int[][]{
+                {1,  0},
+                {3,  2}
+        });
+
+        Solver solver = new Solver(board);
+
+        assertTrue(solver.isSolvable());
+        assertEquals(solver.moves(), 1);
+
+        Iterator<Board> it = solver.solution().iterator();
+        assertTrue(it.hasNext());
+        assertEquals(it.next(), board);
+
+        assertTrue(it.hasNext());
+        assertEquals(it.next(), new Board(new int[][]{
+                {1,  2},
+                {3,  0}
+        }));
+
+        assertFalse(it.hasNext());
+    }
+
+    @Test
+    public void puzzle03() {
+        Board board = new Board(new int[][]{
+                {2,  0},
+                {1,  3}
+        });
+        Solver solver = new Solver(board);
+
+        assertTrue(solver.isSolvable());
+        assertEquals(solver.moves(), 3);
+
+        Iterator<Board> it = solver.solution().iterator();
+        assertTrue(it.hasNext());
+        assertEquals(it.next(), board);
+
+        assertTrue(it.hasNext());
+        assertEquals(it.next(), new Board(new int[][]{
+                {0,  2},
+                {1,  3}
+        }));
+
+        assertTrue(it.hasNext());
+        assertEquals(it.next(), new Board(new int[][]{
+                {1,  2},
+                {0,  3}
+        }));
+
+        assertTrue(it.hasNext());
+        assertEquals(it.next(), new Board(new int[][]{
+                {1,  2},
+                {3,  0}
+        }));
+
+        assertFalse(it.hasNext());
+    }
+
+    @Test
+    public void puzzle04() {
         int[][] blocks = new int[][] {
                 {0, 1, 3},
                 {4, 2, 5},
@@ -70,6 +170,9 @@ public class SolverTest {
         };
 
         Solver solver = new Solver(new Board(blocks));
+
+        assertTrue(solver.isSolvable());
+        assertEquals(solver.moves(), 4);
 
         Iterator<Board> it = solver.solution().iterator();
 
@@ -112,14 +215,158 @@ public class SolverTest {
     }
 
     @Test
-    public void solutionNone() {
-        int[][] blocks = new int[][]{
-                {1, 2, 3},
-                {4, 5, 6},
-                {8, 7, 0}
-        };
+    public void puzzle06() {
+        Board board = new Board(new int[][]{
+                {0,  1,  2,  3},
+                {5,  6,  7,  4},
+                {9,  10, 11, 8},
+                {13, 14, 15, 12},
+        });
+        Solver solver = new Solver(board);
 
-        Solver solver = new Solver(new Board(blocks));
-        assertEquals(solver.solution(), null);
+        assertTrue(solver.isSolvable());
+        assertEquals(solver.moves(), 6);
+
+        Iterator<Board> it = solver.solution().iterator();
+        assertTrue(it.hasNext());
+        assertEquals(it.next(), board);
+
+        assertTrue(it.hasNext());
+        assertEquals(it.next(), new Board(new int[][]{
+                {1,  0,  2,  3},
+                {5,  6,  7,  4},
+                {9,  10, 11, 8},
+                {13, 14, 15, 12}
+        }));
+
+        assertTrue(it.hasNext());
+        assertEquals(it.next(), new Board(new int[][]{
+                {1,  2,  0,  3},
+                {5,  6,  7,  4},
+                {9,  10, 11, 8},
+                {13, 14, 15, 12}
+        }));
+
+        assertTrue(it.hasNext());
+        assertEquals(it.next(), new Board(new int[][]{
+                {1,  2,  3,  0},
+                {5,  6,  7,  4},
+                {9,  10, 11, 8},
+                {13, 14, 15, 12}
+        }));
+
+        assertTrue(it.hasNext());
+        assertEquals(it.next(), new Board(new int[][]{
+                {1,  2,  3,  4},
+                {5,  6,  7,  0},
+                {9,  10, 11, 8},
+                {13, 14, 15, 12}
+        }));
+
+        assertTrue(it.hasNext());
+        assertEquals(it.next(), new Board(new int[][]{
+                {1,  2,  3,  4},
+                {5,  6,  7,  8},
+                {9,  10, 11, 0},
+                {13, 14, 15, 12}
+        }));
+
+        assertTrue(it.hasNext());
+        assertEquals(it.next(), new Board(new int[][]{
+                {1,  2,  3,  4},
+                {5,  6,  7,  8},
+                {9,  10, 11, 12},
+                {13, 14, 15, 0}
+        }));
+
+        assertFalse(it.hasNext());
+    }
+
+    @Test
+    public void unsolvable2x2() {
+        Solver solver = new Solver(new Board(new int[][]{
+                {1,  0},
+                {2,  3}
+        }));
+
+        assertFalse(solver.isSolvable());
+        assertEquals(solver.moves(), -1);
+        assertNull(solver.solution());
+
+        solver = new Solver(new Board(new int[][]{
+                {0,  1},
+                {2,  3}
+        }));
+
+        assertFalse(solver.isSolvable());
+        assertEquals(solver.moves(), -1);
+        assertNull(solver.solution());
+
+        solver = new Solver(new Board(new int[][]{
+                {3,  2},
+                {1,  0}
+        }));
+
+        assertFalse(solver.isSolvable());
+        assertEquals(solver.moves(), -1);
+        assertNull(solver.solution());
+    }
+
+    @Test
+    public void unsolvable3x3() {
+        Solver solver = new Solver(new Board(new int[][]{
+                {1,  2,  3},
+                {4,  6,  5},
+                {7,  8,  0}
+        }));
+
+        assertFalse(solver.isSolvable());
+        assertEquals(solver.moves(), -1);
+        assertNull(solver.solution());
+
+        solver = new Solver(new Board(new int[][]{
+                {8,  6,  7},
+                {2,  5,  4},
+                {1,  3,  0}
+        }));
+
+        assertFalse(solver.isSolvable());
+        assertEquals(solver.moves(), -1);
+        assertNull(solver.solution());
+
+        solver = new Solver(new Board(new int[][]{
+                {1,  2,  3},
+                {4,  5,  6},
+                {8,  7,  0}
+        }));
+
+        assertFalse(solver.isSolvable());
+        assertEquals(solver.moves(), -1);
+        assertNull(solver.solution());
+    }
+
+    @Test
+    public void unsolvable4x4() {
+        Solver solver = new Solver(new Board(new int[][]{
+                {1,  2,  3,  4},
+                {5,  6,  7,  8},
+                {9,  10, 11, 12},
+                {13, 15, 14, 0}
+        }));
+
+        assertFalse(solver.isSolvable());
+        assertEquals(solver.moves(), -1);
+        assertNull(solver.solution());
+
+        solver = new Solver(new Board(new int[][]{
+                {3, 2,  4,  8},
+                {1, 6,  0,  12},
+                {5, 10, 7,  11},
+                {9, 13, 14, 15}
+        }));
+
+        assertFalse(solver.isSolvable());
+        assertEquals(solver.moves(), -1);
+        assertNull(solver.solution());
     }
 }
